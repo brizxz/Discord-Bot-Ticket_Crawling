627cc8d547da628ad2e748a5996028ac79923baf
brizxz <yussp6969@gmail.com>
2024-07-27 19:43:04 +0800

    update

--- ticket.py
+++ ticket.py
@@ -7,11 +7,11 @@ import subprocess
 
 from const.ticket import *
 
-def send_notification(event_name):
+def send_notification(event_time, event_name):
     try:
-        subprocess.run(['notify-send', '票務通知', f'{event_name} 現在有票可以購買！'])
+        subprocess.run(['notify-send', '票務通知' , f'{event_time} {event_name} 現在有票可以購買！'])
     except FileNotFoundError:
-        print(f"無法發送通知：{event_name} 現在有票可以購買！")
+        print(f"{event_time} 無法發送通知：{event_name} 現在有票可以購買！")
 
 def check_ticket_availability(url):
     response = requests.get(url)
@@ -19,8 +19,12 @@ def check_ticket_availability(url):
 
     # 尋找所有包含活動信息的表格行
     rows = soup.find_all('tr', class_='gridc fcTxt')
+    i = 0
     for row in rows:
         # 提取日期、活動名稱和狀態
+        if i == 0:
+            i+=1
+            continue
         date = row.find_all('td')[0].text.strip()
         event_name = row.find_all('td')[1].text.strip()
         status_td = row.find_all('td')[3]
@@ -32,8 +36,10 @@ def check_ticket_availability(url):
 
         if status == "Sold out":
             print(f"{date} - {event_name}: 很抱歉，這個活動已經售完。")
+        elif status == "Find ticketsNo tickets available":
+            print(f"{date} - {event_name}: 很抱歉，這個活動已經選購空。")
         else:
-            send_notification(event_name)
+            send_notification(date, event_name)
 
     
 
